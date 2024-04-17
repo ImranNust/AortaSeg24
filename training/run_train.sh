bfa98be47f6b1b74de121370842efbd80c70baea
Muhammad Imran <34628791+ImranNust@users.noreply.github.com>
2024-04-17 10:08:26 -0400

    Update run_train.sh

--- training/run_train.sh
+++ training/run_train.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 module load conda
-# Check if the AortaSeg24 environment exists
-if conda info --envs | grep AortaSeg24_Challenge; then
-    conda activate AortaSeg24_Challenge
-    echo "Environment already exists."
-else
-    # Create the AortaSeg24 environment with Python 3.10
-    conda create -n AortaSeg24_Challenge python=3.10 -y
-    conda activate AortaSeg24_Challenge
+# # Check if the AortaSeg24 environment exists
+# if conda info --envs | grep AortaSeg24_Challenge; then
+#     conda activate AortaSeg24_Challenge
+#     echo "Environment already exists."
+# else
+#     # Create the AortaSeg24 environment with Python 3.10
+#     conda create -n AortaSeg24_Challenge python=3.10 -y
+#     conda activate AortaSeg24_Challenge
 
-    # Install the specified packages
-    pip3 install torch==2.0.0+cu117 torchvision==0.15.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
-    pip install monai[all]
-    pip install SimpleITK
-    pip install pandas
-    pip install tqdm
-    pip install typing-extensions
-fi
+#     # Install the specified packages
+#     pip3 install torch==2.0.0+cu117 torchvision==0.15.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
+#     pip3 install monai[all]
+#     pip3 install SimpleITK
+#     pip3 install pandas
+#     pip3 install tqdm
+#     pip3 install typing-extensions
+# fi
+
 
 # Directories
-ROOT_DIR="../data/"  # Root directory containing the dataset
+ROOT_DIR="./data/"  # Root directory containing the dataset
 SAVED_MODEL_DIR="./best_model/"           # Directory to save the best model
 
 # Output
@@ -35,6 +36,7 @@ NUM_SAMPLES=4                             # Number of samples for evaluation
 # ---------
 
 # Run the training script with the specified parameters
+# export CUDA_LAUNCH_BLOCKING=1
 python train.py \
     --root_dir "$ROOT_DIR" \
     --saved_model_dir "$SAVED_MODEL_DIR" \
@@ -43,11 +45,3 @@ python train.py \
     --max_iterations "$MAX_ITERATIONS" \
     --eval_num "$EVAL_NUM" \
     --num_samples "$NUM_SAMPLES"
-
-
-
-
-# Deactivate environment
-conda deactivate
-
-
